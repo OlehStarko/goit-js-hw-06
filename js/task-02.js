306fe61e73d7edcb7c6076f75181dea660aa6e1d
OlehStarko <starartds@gmail.com>
2022-07-31 19:56:24 +0300

    Task 2 done

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,16 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+const ingredientsLink = document.getElementById("ingredients");
+const ingredientsArr = [...ingredients].map((element) => {
+  const itemsEl = document.createElement("li");
+  itemsEl.classList.add("title");
+  itemsEl.textContent = element;
+  ingredientsLink.appendChild(itemsEl);
+  return itemsEl;
+});
