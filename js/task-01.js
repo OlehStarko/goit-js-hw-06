aa7db87cf63a35601098336a88fc9a9bfe439def
OlehStarko <starartds@gmail.com>
2022-07-31 19:17:44 +0300

    Task 1 done

---
+++ js/task-01.js
@@ -0,0 +1,12 @@
+const itemElements = document.querySelectorAll(".item");
+console.log(`Number of categories: ${itemElements.length}`);
+
+const categoryArr = [...itemElements]
+  .map(
+    (elements) =>
+      `Category: ${elements.firstElementChild.textContent}
+Elements: ${elements.lastElementChild.children.length}`
+  )
+  .join("\n");
+
+console.log(categoryArr);
