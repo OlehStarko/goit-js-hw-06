4678d7a6160f25b39d4d92de74bee7c675c8ede4
OlehStarko <starartds@gmail.com>
2022-07-31 22:38:29 +0300

    Task 2 second variant

--- js/task-02.js
+++ js/task-02.js
@@ -6,11 +6,29 @@ const ingredients = [
   "Herbs",
   "Condiments",
 ];
+// const ingredientsLink = document.getElementById("ingredients");
+// const ingredientsArr = [...ingredients].map((element) => {
+//   const itemsEl = document.createElement("li");
+//   itemsEl.classList.add("title");
+//   itemsEl.textContent = element;
+//   ingredientsLink.appendChild(itemsEl);
+//   return itemsEl;
+// });
+
 const ingredientsLink = document.getElementById("ingredients");
-const ingredientsArr = [...ingredients].map((element) => {
+
+const createIngredientEl = () => {
   const itemsEl = document.createElement("li");
   itemsEl.classList.add("title");
-  itemsEl.textContent = element;
-  ingredientsLink.appendChild(itemsEl);
+
   return itemsEl;
+};
+
+const addIngredientTextContent = ingredients.map((element) => {
+  const addTextContent = createIngredientEl();
+  addTextContent.textContent = element;
+
+  return addTextContent;
 });
+
+ingredientsLink.append(...addIngredientTextContent);
