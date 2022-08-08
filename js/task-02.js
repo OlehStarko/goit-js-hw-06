7dcc98eb4ba06feef8b4d0bd1652d8c78a60aa9b
OlehStarko <starartds@gmail.com>
2022-08-08 14:41:36 +0300

    2

--- js/task-02.js
+++ js/task-02.js
@@ -7,28 +7,6 @@ const ingredients = [
   "Condiments",
 ];
 
-//FIRST VARIANT
-
-// const ingredientsLink = document.getElementById("ingredients");
-
-// const createIngredientEl = () => {
-//   const itemsEl = document.createElement("li");
-//   itemsEl.classList.add("title");
-
-//   return itemsEl;
-// };
-
-// const addIngredientTextContent = ingredients.map((element) => {
-//   const addTextContent = createIngredientEl();
-//   addTextContent.textContent = element;
-
-//   return addTextContent;
-// });
-
-// ingredientsLink.append(...addIngredientTextContent);
-
-//SECOND VARIANT
-
 const ingredientsLink = document.getElementById("ingredients");
 
 const ingredientsArr = [...ingredients].map((element) => {
